6287f075f27a5be4b5e09615d24d5e8a71875e1e
Abishek Aryal <abishekaryal9558@gmail.com>
2025-04-09 11:58:54 -0400

    first-maven-project and first-test-project

---
+++ first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main (String[] args){
+        System.out.println("apple\nmango\nchery\nbanana\ngrapes\nchicken\nmilk\neggs\nwater");
+
+
+
+    }}

5ecf58dec992318779ba27c01414f1c926ecce3f
Abishek Aryal <abishekaryal9558@gmail.com>
2025-04-10 14:00:45 -0400

    Completed Basic Calculator

---
+++ BasicCalculator/src/main/java/com/pluralsight/Excercise3.java
@@ -0,0 +1,40 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class Excercise3 {
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        float num1 = scanner.nextFloat();
+        System.out.print("Enter the second number: ");
+        float num2 = scanner.nextFloat();
+        System.out.println("possible calculations");
+        System.out.println("(A)dd");
+        System.out.println("(S)ubtract");
+        System.out.println("(M)ultiply");
+        System.out.println("(D)ivide");
+
+
+        System.out.print("please select an option: ");
+        String choice = scanner.next();
+
+
+        float result = num1 * num2 ;
+        System.out.printf("%.2f * %.2f = %.2f\n",num1,num2, result);
+
+
+
+
+
+
+    }
+
+}
+
+
+
+

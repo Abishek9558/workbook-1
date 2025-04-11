687216fd9d73a2b0bdf44b6dde60b18368f9a6cd
Abishek Aryal <abishekaryal9558@gmail.com>
2025-04-11 14:27:50 -0400

    Addes Loaded option for the sandwich shop

--- SandwichShop/src/main/java/com/pluralsight/SandwichShop.java
+++ SandwichShop/src/main/java/com/pluralsight/SandwichShop.java
@@ -1,24 +1,30 @@
 package com.pluralsight;
 import java.util.Scanner;
-
 public class SandwichShop {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.println("1: Regular ($5.45)");
         System.out.println("2: Large ($8.95)");
         System.out.print("sandwich size (1 or 2):");
         int size = scanner.nextInt();
-
         double basePrice = 0.0;
-
-
+        double loadedPrice = 0.0;
 
         if (size == 1){ basePrice = 5.45;
+            loadedPrice = 1.00;
         }
         else if (size == 2) { basePrice = 8.95;
+            loadedPrice = 1.75;
         }
+        System.out.print("you wanna load your sandwich? (yes/no): ");
+        scanner.nextLine();
+        String loadedChoice = scanner.nextLine();
 
+        if (loadedChoice.equals("yes")){basePrice += loadedPrice;}
+        else if (loadedChoice.equals("no")) {
+            System.out.println("NO loaded option.");
+
+        }
 
 
         System.out.println("Enter your age: ");
@@ -29,10 +35,7 @@ public class SandwichShop {
         else if (age >=65) { discount =0.20;}
 
         double finalPrice = basePrice - (basePrice * discount);
-
-
         System.out.printf("sandwich costs: $%.2f%n", finalPrice);
-
         scanner.close();
 
 

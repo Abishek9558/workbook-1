00a22f13bfa4e6811335085efc47a34b59c9e066
Abishek Aryal <abishekaryal9558@gmail.com>
2025-04-11 12:09:40 -0400

    completed mod4 - Excercise 1 - Sandwich shop

---
+++ SandwichShop/src/main/java/com/pluralsight/SandwichShop.java
@@ -0,0 +1,40 @@
+package com.pluralsight;
+import java.util.Scanner;
+
+public class SandwichShop {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("1: Regular ($5.45)");
+        System.out.println("2: Large ($8.95)");
+        System.out.print("sandwich size (1 or 2):");
+        int size = scanner.nextInt();
+
+        double basePrice = 0.0;
+
+
+
+        if (size == 1){ basePrice = 5.45;
+        }
+        else if (size == 2) { basePrice = 8.95;
+        }
+
+
+
+        System.out.println("Enter your age: ");
+        int age = scanner.nextInt();
+
+        double discount = 0.0;
+        if (age <=17) {discount = 0.10;}
+        else if (age >=65) { discount =0.20;}
+
+        double finalPrice = basePrice - (basePrice * discount);
+
+
+        System.out.printf("sandwich costs: $%.2f%n", finalPrice);
+
+        scanner.close();
+
+
+    }
+}
